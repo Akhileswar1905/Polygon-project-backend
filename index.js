1ca9fcb8075a4715f63f7e4a6e567defc759b8c3
Akhileswar <akhileswarsathivada@gmail.com>
2024-04-19 11:04:19 +0530

    commit 1

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function authToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = req.body;
     next();
