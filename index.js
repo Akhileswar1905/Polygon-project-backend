5826c8557b79c9d1c252391eb2108b9a9d21419c
Akhileswar <akhileswarsathivada@gmail.com>
2024-04-19 11:13:56 +0530

    commit 1

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 // });
 
 app.listen(5050, async (req, res) => {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect("mongodb://localhost:27017/");
   console.log("Server listening on http://localhost:5050");
 });
 
@@ -41,9 +41,13 @@ function authToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = req.body;
-    next();
-  });
+  jwt.verify(
+    token,
+    "f3bb8ed3ef82c863aed009900551a761bc8238718b83d7da5e8950d70d640239a17364566379a0a976033f2bc074610bc880321c1a8edbb212948dce0f676f732875057ab14fef37addacd4495a2e306094734009f3a21f52438dbba2a627e78a8ef1cac",
+    (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = req.body;
+      next();
+    }
+  );
 }
