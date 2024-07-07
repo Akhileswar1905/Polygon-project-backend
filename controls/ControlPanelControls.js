400a47b6d967dc8500213af42519935f3c2476c7
Akhileswar <akhileswarsathivada@gmail.com>
2024-07-07 17:10:20 +0530

    commit 16

--- controls/ControlPanelControls.js
+++ controls/ControlPanelControls.js
@@ -294,6 +294,9 @@ const acceptUpdate = async (req, res) => {
       { phoneNumber: phoneNumber, "tripDetails.tripID": tripId },
       { $set: { "tripDetails.$.status": "allowed" } }
     );
+    const cp = await ControlPanel.findOne({ _id: cpId });
+
+    res.status(200).json(cp.updates);
   } catch (error) {
     console.log(error.message);
     res.status(500).send(error.message);
